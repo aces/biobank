0d6ba431a306575068bc83e545441e25cb899f3c
Henri Rabalais <henri.j.rabalais@gmail.com>
2025-04-25 09:44:02 -0400

    removed console log

--- jsx/customFields.js
+++ jsx/customFields.js
@@ -18,7 +18,6 @@ function CustomFields(props) {
 
   return attributes.map(
     (attribute, key) => {
-      console.log(attribute.label);
       const datatype = options.specimen.attributeDatatypes[attribute.datatypeId]
         .datatype;
       if (datatype === 'text' || datatype === 'number') {
